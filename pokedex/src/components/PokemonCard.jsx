0a93df5c819b85ce6a3b955b93b27a4250ea44a7
Landry1602 <landrymaillard@gmail.com>
2024-03-28 11:43:52 +0100

    added conditionnal display function

--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -1,12 +1,25 @@
 function PokemonCard() {
+    let pokemon = pokemonList[0]
     return (
       <div>
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-            <figcaption>Bulbasaur</figcaption>
+            {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} alt={pokemon.name}/>: <p>???</p>}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
       </div>
     );
 };
+
+
+const pokemonList = [
+    {
+      name: "Bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "Mew",
+    },
+  ];
 
 export default PokemonCard;
