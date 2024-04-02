d1fc25256228dea40a36d03ff741a7e8e74f944a
Landry1602 <landrymaillard@gmail.com>
2024-04-02 09:29:52 +0200

    adding props to PokemonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,26 @@
+function PokemonCard(props) {
+  console.log(props)
+  let pokemon = pokemonList[0]
+  return (
+    <div>
+      <figure>
+          {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} alt={pokemon.name}/>: <p>???</p>}
+          <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </div>
+  );
+};
+
+
+const pokemonList = [
+    {
+      name: "Bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "Mew",
+    },
+  ];
+
+export default PokemonCard;
